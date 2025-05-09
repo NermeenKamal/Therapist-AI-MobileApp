dcd75f16964fa8e3b1e0b2de5d4e64a3f8a9fe8f
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-05-10 00:29:22 +0300

    Update database.php

--- Backend/config/database.php
+++ Backend/config/database.php
@@ -43,25 +43,22 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => [
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/cert.pem'), // Default path on Railway
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ],
-        ],
+    'driver' => 'mysql',
+    'host' => env('DB_HOST', 'yamanote.proxy.rlwy.net'),
+    'port' => env('DB_PORT', '41769'),
+    'database' => env('DB_DATABASE', 'railway'),
+    'username' => env('DB_USERNAME', 'root'),
+    'password' => env('DB_PASSWORD', 'rBnjLvNQFchQHxkBFxAKCZBHCkYzNYVv'),
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => true,
+    'engine' => null,
+    'options' => [
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/cert.pem'), // Railway's default CA
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Disable certificate validation
+    ],
+],
 
         'mariadb' => [
             'driver' => 'mariadb',
