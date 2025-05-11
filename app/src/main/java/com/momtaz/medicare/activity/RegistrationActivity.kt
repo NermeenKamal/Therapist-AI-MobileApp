072fff96b017e85564fc3d56976f80de4a34cea1
Abdelrhman Momtaz <140316365+Abdelrhmanmomtaz58@users.noreply.github.com>
2025-05-11 20:03:32 +0300

    Add frontend project inside Frontend folder

---
+++ app/src/main/java/com/momtaz/medicare/activity/RegistrationActivity.kt
@@ -0,0 +1,14 @@
+package com.momtaz.medicare.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.momtaz.medicare.R
+
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+    }
+}
