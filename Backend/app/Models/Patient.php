535c8a0c5d7b3b7f65c439a72e5607f212b45eee
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-05-16 20:17:34 +0300

    Update Patient.php

--- Backend/app/Models/Patient.php
+++ Backend/app/Models/Patient.php
@@ -2,26 +2,46 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Patient extends Model
+class Patient extends Authenticatable
 {
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'patients';
+
     protected $fillable = [
-        'user_id',
+        'name',
+        'email',
+        'password',
+        'mobile_number',
+        'national_id',
+        'date_of_birth',
+        'gender',
+        'medical_history',
+        'current_medications',
+        'allergies',
+        'emergency_contact_name',
+        'emergency_contact_number',
+        'profile_image',
+        'fcm_token'
     ];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'date_of_birth' => 'date',
+        'email_verified_at' => 'datetime',
+    ];
 
     public function appointments()
     {
-        return $this->hasMany(Appointment::class);
-    }
-
-    public function reports()
-    {
-        return $this->hasMany(PatientReport::class);
+        return $this->hasMany(Appointment::class, 'patient_id');
     }
 } 
