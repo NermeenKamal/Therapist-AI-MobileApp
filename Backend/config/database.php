d4331a66276afdae9be295b0666e583f83aeb55e
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-05-16 17:24:03 +0300

    Update database.php

--- Backend/config/database.php
+++ Backend/config/database.php
@@ -44,17 +44,23 @@ return [
 
        'mysql' => [
     'driver' => 'mysql',
-    'host' => env('DB_HOST'),
-    'port' => env('DB_PORT'),
-    'database' => env('DB_DATABASE'),
-    'username' => env('DB_USERNAME'),
-    'password' => env('DB_PASSWORD'),
+    'url' => env('MYSQL_URL'),
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_DATABASE', 'laravel'),
+    'username' => env('DB_USERNAME', 'root'),
+    'password' => env('DB_PASSWORD', ''),
+    'unix_socket' => env('DB_SOCKET', ''),
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
-    'options' => [
-        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'strict' => true,
+    'engine' => null,
+    'options' => extension_loaded('pdo_mysql') ? array_filter([
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/cert.pem'),
         PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-    ],
+    ]) : [],
 ],
 
         'mariadb' => [
