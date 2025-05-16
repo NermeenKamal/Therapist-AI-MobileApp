e4e9ada534ce034e5719abe7ebc47aba08d3f118
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-05-16 17:25:39 +0300

    Add files via upload

---
+++ Backend/app/Models/Doctor.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Doctor extends Authenticatable
+{
+    use HasApiTokens, Notifiable, HasFactory;
+
+    protected $table = 'doctors';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'mobile_number',
+        'national_id',
+        'specialization',
+        'bio',
+        'session_price',
+        'medical_license_path',
+        'profile_image',
+        'fcm_token',
+        'is_verified',
+        'is_verified_by_ocr'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'session_price' => 'decimal:2',
+        'is_verified' => 'boolean',
+        'is_verified_by_ocr' => 'boolean',
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class, 'doctor_id');
+    }
+} 
