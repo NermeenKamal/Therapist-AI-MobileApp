1b1539ce9de20ca5b6675dab9931d31bfe9dc3f6
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-05-16 15:31:53 +0300

    Add files via upload

---
+++ Backend/app/Controllers/ReportGenerationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Services\ReportGenerationService;
+
+class ReportGenerationController extends Controller
+{
+    protected ReportGenerationService $reportService;
+
+    public function __construct(ReportGenerationService $reportService)
+    {
+        $this->reportService = $reportService;
+    }
+
+    public function generate(Request $request): JsonResponse
+    {
+        $request->validate([
+            'patient_id' => 'required|integer|exists:users,id',
+            'conversation_log' => 'required|array',
+        ]);
+
+        $url = $this->reportService->generatePdf(
+            $request->input('patient_id'),
+            $request->input('conversation_log')
+        );
+
+        return response()->json(['report_pdf_url' => $url]);
+    }
+}
