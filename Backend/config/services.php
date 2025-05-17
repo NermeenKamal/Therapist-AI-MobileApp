eac0419027b0e7785db56369ea327984a315856c
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-05-17 17:11:23 +0300

    Update services.php

--- Backend/config/services.php
+++ Backend/config/services.php
@@ -13,4 +13,25 @@ return [
         'bert_endpoint' => env('BERT_SENTIMENT_ENDPOINT', 'https://therapist-ai-mobileapp-production.up.railway.app/'),
     ],
 
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'sendgrid' => [
+        'key' => env('SENDGRID_API_KEY'),
+    ],
+
 ];
