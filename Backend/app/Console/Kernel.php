eaeedcb5951ff1b05df4f01c9ba7b707f030fbaf
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-05-17 23:33:44 +0300

    Update Kernel.php

--- Backend/app/Console/Kernel.php
+++ Backend/app/Console/Kernel.php
@@ -19,8 +19,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        // تشغيل استيراد المقالات يومياً في الساعة 12 صباحاً
-        $schedule->command('import:articles')->dailyAt('00:00');
+        $schedule->job(new ImportArticlesJob)->weekly();
     }
 
     protected function commands(): void
