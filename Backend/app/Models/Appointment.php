19fdc0b0698914f495044cc6577f55a736d76f56
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-05-18 21:38:32 +0300

    Update Appointment.php

--- Backend/app/Models/Appointment.php
+++ Backend/app/Models/Appointment.php
@@ -5,27 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Doctor;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use App\Models\DoctorSchedule;
 
 class Appointment extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'patient_id', 'doctor_id', 'appointment_date', 'status', 'notes', 'price'
+        'patient_id',
+        'doctor_id',
+        'appointment_date',
+        'status',
+        'notes',
+        'price',
     ];
 
-    protected $dates = ['date'];
+    protected $dates = ['appointment_date'];
 
-    public final function patient() : BelongsTo
+    public final function patient(): BelongsTo
     {
         return $this->belongsTo(User::class, 'patient_id');
     }
 
-    public final function doctor() : BelongsTo
+    public final function doctor(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'doctor_id');
+        return $this->belongsTo(Doctor::class, 'doctor_id');
     }
-
 }
