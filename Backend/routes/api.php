bfbb3d5e20c6c2d706aa7ec6f10ef4b7e77ca950
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-05-20 18:44:53 +0300

    Update api.php

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -14,7 +14,7 @@ use App\Controllers\AIChatbotController;
 use App\Controllers\DoctorScheduleController;
 use App\Controllers\DoctorController;
 use App\Controllers\ArticleController;
-use App\Controllers\FCMController;
+use App\Controllers\fcm_controller;
 
 // reset password routes
 Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetCode']);
