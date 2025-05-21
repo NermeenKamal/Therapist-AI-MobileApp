5a3dd162148008b0813fd7e88a869c6e85a3d248
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-05-22 01:01:49 +0300

    Update Appointment.php

--- Backend/app/Models/Appointment.php
+++ Backend/app/Models/Appointment.php
@@ -16,6 +16,7 @@ class Appointment extends Model
     public const STATUS_BOOKED = 'booked';
     public const STATUS_CANCELED = 'canceled';
     public const STATUS_PENDING = 'pending';
+    public const STATUS_CONFIRMED = 'confirmed';
 
     protected $fillable = [
         'patient_id',
