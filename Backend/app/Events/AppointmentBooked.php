f0bd49212e050e1c717e95ab00a27762bbff51f0
camilla0olkl <143268530+camilla0olkl@users.noreply.github.com>
2025-06-11 15:59:54 +0300

    Update AppointmentBooked.php

--- Backend/app/Events/AppointmentBooked.php
+++ Backend/app/Events/AppointmentBooked.php
@@ -1,1 +1,14 @@
-<?php namespace App\Events;use App\Models\Appointment;use Illuminate\Broadcasting\InteractsWithSockets;use Illuminate\Foundation\Events\Dispatchable;use Illuminate\Queue\SerializesModels;class AppointmentBooked{    use Dispatchable, InteractsWithSockets, SerializesModels;    public function __construct(public Appointment $appointment) {}}
+<?php 
+namespace App\Events;
+
+use App\Models\Appointment;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class AppointmentBooked
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public Appointment $appointment) {}
+}
